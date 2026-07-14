85d8165afe97594a4e570b23c299e6461a8725c1
Rupak Sarkar <rs9091754723@gmail.com>
2026-07-14 13:24:18 +0900

    Update education.ts

--- data/education.ts
+++ data/education.ts
@@ -1,32 +1,32 @@
 const education = [
   {
-    year: "2023 - Present",
-    degree: "BS-MS in Chemistry",
-    institute: "Indian Institute of Science Education and Research (IISER) Kolkata",
-    grade: "CGPA: 9.07",
+    year: "2023 – Present",
+    degree: "BS–MS in Chemistry",
+    institute:
+      "Indian Institute of Science Education and Research (IISER) Kolkata",
+    grade: "CGPA: 9.07/10.00",
     description:
-      "Major in Chemistry and minor in Biology with research interests in heterogeneous catalysis, surface chemistry, organic chemistry, and bio-chemistry.",
+      "Pursuing an integrated BS–MS with a major in Chemistry and a minor in Biology. Interested in research focusing on heterogeneous catalysis, surface chemistry, organic synthesis, and biomolecular chemistry.",
   },
 
-  
   {
     year: "2023",
-    degree: "Higher Secondary",
+    degree: "Higher Secondary (Class XII)",
     institute: "Fulbari High School (H.S.)",
     board: "West Bengal Council of Higher Secondary Education (WBCHSE)",
     grade: "95.8%",
     description:
-      "Mathematics, Physics, Chemistry, Biology, English, and Bengali",
+      "Graduated with Mathematics, Physics, Chemistry, Biology, English, and Bengali.",
   },
 
   {
     year: "2021",
-    degree: "Secondary",
+    degree: "Secondary (Class X)",
     institute: "Fulbari High School",
-    board: "West Bengal Board of Secondary Education",
+    board: "West Bengal Board of Secondary Education (WBBSE)",
     grade: "97.28%",
-    description"
-      "Mathematics, Physical Science, Life Science, Geography, History, English, and Bengali",
+    description:
+      "Completed secondary education with Mathematics, Physical Science, Life Science, Geography, History, English, and Bengali.",
   },
 ];
 
